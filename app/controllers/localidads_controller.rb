c2c44e06177f4bed957407cce302aa952fccc302
pellytw <marcelo.eduardo.gonzalez@gmail.com>
2014-05-29 09:04:09 -0300

    navbar fixed top

--- app/controllers/localidads_controller.rb
+++ app/controllers/localidads_controller.rb
@@ -40,6 +40,7 @@ class LocalidadsController < ApplicationController
   # POST /localidads
   # POST /localidads.json
   def create
+    debugger
     @localidad = Localidad.new(params[:localidad])
 
     respond_to do |format|
